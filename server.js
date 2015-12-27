3a8e43860437c4ef23b79b304f306784d98d85f2
Jeff Jing <geekingin@gmail.com>
2015-12-27 15:36:54 +0800

    init server

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+// set up
+var express = require('express')
+var mongoose = require('mongoose')
+var morgan = require('morgan')
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
+
+var app = express()
+// configuration
+
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodules.net:27017/uwO3mypu')
+app.use(express.static(__dirname + '/public'))
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({'extended': 'true'}))					//todo
+app.use(bodyParser.json())												//todo
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))			//todo
+app.use(methodOverride())
+
+// start server
+app.listen(8080)
+console.log('App listening on port 8080')
